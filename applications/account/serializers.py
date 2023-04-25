7e2c7d20eec54d44d539df85745007c71854f23f
NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
2023-04-25 22:47:24 +0600

    add new app and fixed code

--- applications/account/serializers.py
+++ applications/account/serializers.py
@@ -3,13 +3,14 @@ from rest_framework import serializers
 from .models import CustomUser
 from .send_email import send_activation_code, send_reset_password_code
 
+
 # User = get_user_model()  # CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(
         required=True,
         min_length=6,
-        write_only=True 
+        write_only=True
     )
 
     class Meta:
@@ -18,7 +19,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def validate_email(self, email):
         return email
-    
+
     def validate(self, attrs):
         p1 = attrs.get('password')
         p2 = attrs.pop('password2')
@@ -27,12 +28,13 @@ class RegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Password did not match!!!')
 
         return attrs
-    
+
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
         send_activation_code(user.email, user.activation_code)
         return user
-    
+
+
 class ForgotPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
 
@@ -40,13 +42,14 @@ class ForgotPasswordSerializer(serializers.Serializer):
         if not CustomUser.objects.filter(email=email).exists():
             raise serializers.ValidationError('пользователь с такой почтой не существует')
         return email
-    
+
     def send_reset_password_code(self):
         email = self.validated_data.get('email')
         user = CustomUser.objects.get(email=email)
         user.create_activation_code()
         user.save()
         send_reset_password_code(email=email, code=user.activation_code)
+
 
 class ForgotPasswordComleteSerializer(serializers.Serializer):
     password = serializers.CharField(required=True, min_length=6)
@@ -59,12 +62,12 @@ class ForgotPasswordComleteSerializer(serializers.Serializer):
         if p1 != p2:
             raise serializers.ValidationError('пароли не совпадают')
         return attrs
-    
+
     def validate_code(self, code):
         if not CustomUser.objects.filter(activation_code=code).exists():
             raise serializers.ValidationError('неверный код')
         return code
-    
+
     def set_new_password(self):
         user = CustomUser.objects.get(activation_code=self.validated_data.get('code'))
         password = self.validated_data.get('password')
---
+++ restaurants/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Restaurants(models.Model):
+    title = models.CharField(max_length=155, )
+    image = models.ImageField(upload_to='restaurant-image', )
+    price_people = models.CharField(max_length=225)
+    locate = models.URLField()
+    working_hours = models.CharField(max_length=200)
+    features = models.TextField(max_length=1000)
