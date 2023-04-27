afd0ef393c7c0176c9c8a17a093f1321f5bd0d86
NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
2023-04-27 19:42:32 +0600

    add permission and cheks

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,13 +1,21 @@
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Restaurants
+from .permissions import IsBusinessUser
 from .serializers import RestaurantSerializer
 
 
 class CreateRestaurant(CreateAPIView):
     queryset = Restaurants.objects.all()
     serializer_class = RestaurantSerializer
+    permission_classes = [IsAuthenticated, IsBusinessUser]
+
+    def perform_create(self, serializer):
+        serializer.save(business=self.request.user)  # Сохранение ресторана с указанием пользователя
 
 
 class RetrieveUpdateDestroyRestaurant(RetrieveUpdateDestroyAPIView):
     queryset = Restaurants.objects.all()
     serializer_class = RestaurantSerializer
+    permission_classes = [IsAuthenticated, IsBusinessUser]
