85d17b7eb9bd7c6b54f59dfd4a732dce07c187e3
Aimerim <aimeerimt68@gmail.com>
2023-05-01 21:09:06 +0600

    панель admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import Restaurant, Favorite
 
 
 @admin.register(Restaurant)
@@ -7,3 +7,6 @@ class RestaurantsAdmin(admin.ModelAdmin):
     list_display = ['name', 'image', 'price_people', 'locate', 'working_hours', 'features']
 
 
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ['user', 'item', 'date_added']
--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Restaurant, Favorite
 
 
-
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
